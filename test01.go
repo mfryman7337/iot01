a207e37df63b26932f7ef43026c76d17ff216a16
Michael Fryman <michael@fryman.org>
2020-02-24 12:09:43 -0800

    commit by MFryman 202002241209

--- test01.go
+++ test01.go
@@ -7,8 +7,6 @@ import (
 
 func main() {
 	fmt.Println("Welcome to the playground!")
-
 	fmt.Println("The time is", time.Now())
-
-	fmt.Println("Just another line")
+	fmt.Println("Line added 20200224:1209")
 }
