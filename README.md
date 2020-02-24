2a2c1dc9bf5c7a75ae10462802acd7053295fea5
Michael Fryman <michael@fryman.org>
2020-02-24 12:46:43 -0800

    MF commit from CbrW530W10_VM_UbuntuDevOps 20200224:1245

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 IoT repo 01
 
 First repo to test out IoT tutorials and try stuff out.
+changed a line from MFpcW10_VM_UbuntuDevOps on 20200224:1209
+changed a line from CbrW530W10_VM_UbuntuDevOps on 20200224:1245
--- test01.go
+++ test01.go
@@ -8,5 +8,6 @@ import (
 func main() {
 	fmt.Println("Welcome to the playground!")
 	fmt.Println("The time is", time.Now())
-	fmt.Println("Line added 20200224:1209")
+	fmt.Println("Line added MFpcW10_VM_UbuntuDevOps 20200224:1209")
+	fmt.Println("Line added CbrW530W10_VM_UbuntuDevOps 20200224:1245")
 }
